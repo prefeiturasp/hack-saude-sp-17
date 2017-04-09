40eedd951a3489f6c17312c42ff7eac19bb5e669
Vinícius Guerra Cardoso <dev.viniciusguerra@gmail.com>
2017-04-09 01:31:47 -0300

    Bring _future_ to the top of the script.

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from __future__ import print_function
 
 from flask import Flask
 from flask import request
@@ -12,7 +13,7 @@ from urllib.error import HTTPError
 import json
 import os
 
-from __future__ import print_function
+
 from future.standard_library import install_aliases
 install_aliases()
 
